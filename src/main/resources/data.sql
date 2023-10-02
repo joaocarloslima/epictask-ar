7e384ee6318a4519d35c7976507585a330dfcd82
Joao <joaocarloslima@me.com>
2023-10-02 20:50:07 -0300

    criação do projeto

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO TASK (title, description, score) VALUES ('Prototipo', 'Criar no figma', 50);
+INSERT INTO TASK (title, description, score, status) VALUES ('BD', 'Modelar o banco de dados', 100, 30);
+INSERT INTO TASK (title, description, score) VALUES ('Testes', 'Fazer os testes unitarios', 20);
